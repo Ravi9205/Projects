8ba75581edbde500327961be948fa16d655e7f0d
Ravi dwivedi <ravi.dwivedi9205@gmail.com>
2023-05-16 17:01:31 +0530

    New Changes Latest

---
+++ ToDoListSwiftUI/ToDoListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoListSwiftUI
+//
+//  Created by Ravi Dwivedi on 15/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LoginView()
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
